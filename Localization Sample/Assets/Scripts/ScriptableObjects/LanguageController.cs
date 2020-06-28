932e1e282289a7ce7e173d712c89c0650de9a8d3
WhiteBeard <mrgameoz@gmail.com>
2020-06-28 19:01:09 +0530

    removed unwanted code

--- Localization Sample/Assets/Scripts/ScriptableObjects/LanguageController.cs
+++ Localization Sample/Assets/Scripts/ScriptableObjects/LanguageController.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LanguageController", menuName = "ScriptableObjects/LanguageController", order = 1)]
 public class LanguageController : ScriptableObject
 {
-    public int languageIndex = 0;
     public List<LanguageData> languageDatas = new List<LanguageData>();
 }
